5cee8ec04ac8616729a733512aeedd216ebab3ca
Crilofer <cristian@tymit.com>
2020-04-19 13:57:43 +0200

    Updated Quarkus platform version and added test

--- src/test/java/com/crilofer/quickstart/GreetingResourceTest.java
+++ src/test/java/com/crilofer/quickstart/GreetingResourceTest.java
@@ -3,6 +3,8 @@ package com.crilofer.quickstart;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -12,10 +14,19 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("hello"));
     }
 
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+                .when().get("/hello/greeting/cristian")
+                .then()
+                .statusCode(200)
+                .body(is("hello cristian"));
+    }
 }
